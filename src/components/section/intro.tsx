a96fdb61de6db616957d1f194eceb65325e8709d
LeeHueeng <zzxx373014@gmail.com>
2023-11-07 10:11:30 +0900

    구상잡기 위해 더미이미지 추가

--- src/components/section/intro.tsx
+++ src/components/section/intro.tsx
@@ -3,15 +3,26 @@ import { css } from "@emotion/react";
 import bgLottie from "assets/lottieJSON/wave.json";
 import LottieWrapper from "components/lottieWrapper";
 import theme from "styles/theme/theme";
+import developimg from "assets/images/develop.png";
+import designimg from "assets/images/design.png";
+import Image from "next/image";
 
 export default function Intro() {
   return (
     <section css={wrapper}>
       <div css={inWrapper}>
-        <div css={contentWrapper}>
-          <h1 css={title}>CodingBottle</h1>
-          <p css={subTitle}>끊임없이 흘러가는 우리</p>
-          <p css={subTitle}>Developer X Designer 코딩보틀</p>
+        <div css={main}>
+          <div css={contentWrapper}>
+            <h1 css={title}>CodingBottle</h1>
+            <p css={subTitle}>끊임없이 흘러가는 우리</p>
+            <p css={subTitle}>Developer X Designer 코딩보틀</p>
+          </div>
+          <div css={img_1}>
+            <Image src={developimg} alt="developimg" />
+          </div>
+          <div css={img_2}>
+            <Image src={designimg} alt="designimg" />
+          </div>
         </div>
       </div>
     </section>
@@ -19,7 +30,6 @@ export default function Intro() {
 }
 
 const wrapper = css`
-  position: relative;
   width: 100%;
   height: 100%;
 `;
@@ -29,10 +39,11 @@ const inWrapper = css`
 `;
 
 const contentWrapper = css`
-  display: flex;
-  flex-direction: column;
-  align-items: left;
+  position: absolute;
+  z-index: 3;
   margin-top: 10%;
+  top: 10%;
+  left: 10%;
 `;
 
 const title = css`
@@ -45,4 +56,24 @@ const subTitle = css`
   font-size: 3rem;
   color: ${theme.color.white};
   font-weight: ${theme.fontWeight.light};
+`;
+const img_1 = css`
+  z-index: 2;
+  position: absolute;
+  right: 40%;
+  top: 40%;
+  width: 400px;
+  height: 400px;
+`;
+const img_2 = css`
+  z-index: 1;
+  position: absolute;
+  left: 60%;
+  top: 20%;
+  width: 400px;
+  height: 400px;
+`;
+const main = css`
+  width: 100%;
+  height: 100%;
 `;
